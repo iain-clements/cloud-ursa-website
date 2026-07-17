1ae3894dc538f6d0379a4f9a2dafa147e6403488
Iain Clements <iain@notbinary.co.uk>
2026-07-17 10:10:48 +0100

    updated quiz, style, blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,12 @@
-/* * Version: 0.7 
- * Date: 2026-07-16 18:20
- */
+// src/content.config.ts
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "src/content/blog" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
-    pubDate: z.coerce.date(),
-    description: z.string(),
+    description: z.string().optional(),
   }),
 });
 
